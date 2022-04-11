9b84b1931f68eb662b4e482b6bec1167492b9a67
Viney <vineykhaneja999@gmail.com>
2022-04-11 10:40:25 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+        @Author: Viney Khaneja
+        @Date: 2022-04-11 08:54AM
+        @Last Modified by: Viney Khaneja
+        @Last Modified time: None
+        @Title : EmpWage Problem Statements
+    '''
+
+print("Welcome to EmpWage Program in Python")

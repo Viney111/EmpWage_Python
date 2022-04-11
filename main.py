d8b88b6e9b6fb88a48b784514f49b91912684381
Viney <vineykhaneja999@gmail.com>
2022-04-11 11:01:44 +0530

    [Viney] ADDED: Calculating daily EmpWage

--- main.py
+++ main.py
@@ -10,10 +10,24 @@ print("Welcome to EmpWage Program in Python")
 
 #Importing Random
 import random
-IS_PRESENT = 1
 
-emp_check = random.randint(0,1)
-if emp_check == IS_PRESENT:
-    print("Employee is present")
-else:
-    print("Employee is absent")
+#CONSTANTS
+IS_PRESENT = 1
+WAGE_PER_HOUR = 20
+FULL_DAY_HOUR = 8
+
+def get_work_hours():
+    '''
+            Description: Function to get Employee Working hours
+            Parameter: None
+            Return: work_hrs as per Random Calculation
+        '''
+    emp_check = random.randint(0,1)
+    if emp_check == IS_PRESENT:
+        work_hrs = FULL_DAY_HOUR
+    else:
+        work_hrs = 0
+    return work_hrs
+
+daily_emp_wage = WAGE_PER_HOUR * get_work_hours()
+print(f"Person earns {daily_emp_wage} rupees this day")

ae30eccfb37bfb545aa7314ea85f34652daf1926
Viney <vineykhaneja999@gmail.com>
2022-04-11 10:51:51 +0530

    [Viney] ADDED: Checking Employee Present or Absent

--- main.py
+++ main.py
@@ -3,7 +3,17 @@
         @Date: 2022-04-11 08:54AM
         @Last Modified by: Viney Khaneja
         @Last Modified time: None
-        @Title : EmpWage Problem Statements
+        @Title : UC1_Check Employee Present or absent
     '''
 
 print("Welcome to EmpWage Program in Python")
+
+#Importing Random
+import random
+IS_PRESENT = 1
+
+emp_check = random.randint(0,1)
+if emp_check == IS_PRESENT:
+    print("Employee is present")
+else:
+    print("Employee is absent")

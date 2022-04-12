230ea31e253c2b96386b4e197ff52d01859cdcf0
Viney <vineykhaneja999@gmail.com>
2022-04-12 10:33:21 +0530

    [Viney] Refactored: Applying Special variable __name__ condition

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
         @Author: Viney Khaneja
         @Date: 2022-04-11 08:54AM
         @Last Modified by: Viney Khaneja
-        @Last Modified time: None
+        @Last Modified time: 2022-04-12 10:29AM
         @Title : EmpWage Problem Statements
     '''
 
@@ -41,16 +41,25 @@ def get_work_hours():
             work_hrs = 0
     return work_hrs
 
-#Initializing Variables with zero value
-monthly_emp_wage = total_work_hrs = total_work_days = daily_wage = 0
-daily_wage_dict = {}
-#Applying Constions of Max Working days and hours in Whie Loop
-while total_work_hrs <= Constants.MAX_WORK_HRS_PER_MONTH and total_work_days < Constants.MAX_WORKING_DAYS_PER_MONTH:
-    work_hrs_per_day = get_work_hours() #Calling GetWorkHrs Function...
-    daily_wage = work_hrs_per_day * Constants.WAGE_PER_HOUR
-    total_work_hrs += work_hrs_per_day
-    total_work_days += 1
-    daily_wage_dict[total_work_days] = daily_wage
+def calculate_emp_wage():
+    '''
+            Description: Function to calculate monthly and daily employee wage
+            Parameter: None
+            Return: Nothing, just prints the monthly Wage & emp wage day-wise
+        '''
+    #Initializing Variables with zero value
+    monthly_emp_wage = total_work_hrs = total_work_days = daily_wage = 0
+    daily_wage_dict = {}
+    #Applying Conditions of Max Working days and hours in Whie Loop
+    while total_work_hrs <= Constants.MAX_WORK_HRS_PER_MONTH and total_work_days < Constants.MAX_WORKING_DAYS_PER_MONTH:
+        work_hrs_per_day = get_work_hours() #Calling GetWorkHrs Function...
+        daily_wage = work_hrs_per_day * Constants.WAGE_PER_HOUR
+        total_work_hrs += work_hrs_per_day
+        total_work_days += 1
+        daily_wage_dict[total_work_days] = daily_wage
 
-monthly_emp_wage = Constants.WAGE_PER_HOUR * total_work_hrs
-print(f"Person earns {monthly_emp_wage} rupees this month & Daily Emp Wage daywise:\n{daily_wage_dict} respectively.")
+    monthly_emp_wage = Constants.WAGE_PER_HOUR * total_work_hrs
+    print(f"Person earns {monthly_emp_wage} rupees this month & Daily Emp Wage daywise:\n{daily_wage_dict} respectively.")
+
+if __name__ == "__main__":
+    calculate_emp_wage()
